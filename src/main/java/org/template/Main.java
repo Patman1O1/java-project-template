7d3e3b358e458efe553f393354faea7429b0e30d
Patman1O1 <patrickthomas101x@gmail.com>
2026-05-10 15:16:05 -0500

    Edit Main.java

--- src/main/java/org/template/Main.java
+++ src/main/java/org/template/Main.java
@@ -2,12 +2,14 @@ package org.template;
 
 import java.lang.System;
 
-void main() {
-    try {
-        System.out.println("Hello world!");
-    } catch (Exception exception) {
-        exception.printStackTrace();
-        System.exit(1);
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello world");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.exit(1);
+        } 
     }
 }
 
